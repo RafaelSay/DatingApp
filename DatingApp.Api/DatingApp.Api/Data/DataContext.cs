83ec3d9430d76cc2789c8d227c625e84487f7895
RafaelSay <3rsprod@gmail.com>
2020-03-20 11:30:05 -0400

    first commit

---
+++ DatingApp.Api/DatingApp.Api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using DatingApp.Api.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DatingApp.Api/DatingApp.Api/Model/Value.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingApp.Api.Model
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        
+    }
+}
